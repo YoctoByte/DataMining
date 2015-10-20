34409b4a4637420bfd3c6df23112b9d7a139acb8
David van 't Wout <david.vtwout@gmail.com>
2015-10-20 20:52:39 +0200

    decision tree added

--- Homework 3/as_3_1.py
+++ Homework 3/as_3_1.py
@@ -1,4 +1,5 @@
 from scipy.io import loadmat
+from sklearn import tree
 
 filename = 'Data/wine.mat'
 data = loadmat(filename)
@@ -10,7 +11,16 @@ for index in range(len(data['classNames'])):
     classNames.append(data['classNames'][index][0][0])
 for index in range(len(data['attributeNames'][0])):
     attributeNames.append(data['attributeNames'][0][index][0])
-print(classNames)
-print(attributeNames)
-print(attributes)
-print(wineClass)
+
+clf = tree.DecisionTreeClassifier(criterion="gini", min_samples_split=100)
+clf.fit(attributes, wineClass)
+index = 0
+good = 0
+bad = 0
+for sample in attributes:
+    if clf.predict(sample) == wineClass[index]:
+        good += 1
+    else:
+        bad += 1
+    index += 1
+print(good, bad)
