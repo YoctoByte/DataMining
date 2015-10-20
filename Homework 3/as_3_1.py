1991b8febf5b94c5b7c80215c0c5109cc49b8ea9
David van 't Wout <david.vtwout@gmail.com>
2015-10-20 21:22:04 +0200

    removed index variable

--- Homework 3/as_3_1.py
+++ Homework 3/as_3_1.py
@@ -12,15 +12,13 @@ for index in range(len(data['classNames'])):
 for index in range(len(data['attributeNames'][0])):
     attributeNames.append(data['attributeNames'][0][index][0])
 
-clf = tree.DecisionTreeClassifier(criterion="gini", min_samples_split=100)
+clf = tree.DecisionTreeClassifier(criterion="gini", min_samples_split=1)
 clf.fit(attributes, wineClass)
-index = 0
 good = 0
 bad = 0
-for sample in attributes:
-    if clf.predict(sample) == wineClass[index]:
+for sample in enumerate(attributes):
+    if clf.predict(attributes[sample[0]]) == wineClass[sample[0]]:
         good += 1
     else:
         bad += 1
-    index += 1
 print(good, bad)
