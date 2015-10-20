155e48aafb47663e586806b3cfae35360abe0b06
David van 't Wout <david.vtwout@gmail.com>
2015-10-20 20:07:46 +0200

    assignment 3.1 can read file now

--- Homework 3/as_3_1.py
+++ Homework 3/as_3_1.py
@@ -2,4 +2,14 @@ from scipy.io import loadmat
 
 filename = 'Data/wine.mat'
 data = loadmat(filename)
-print (data)
+attributes = data['X']
+tempAttributeNames = data['attributeNames']
+tempClassNames = data['classNames']
+classNames = []
+attributeNames = []
+for index in range(len(tempClassNames)):
+    classNames.append(tempClassNames[index][0][0])
+for index in range(len(tempAttributeNames[0])):
+    attributeNames.append(tempAttributeNames[0][index][0])
+print(classNames)
+print(attributeNames)
