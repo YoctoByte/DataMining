d9b3bb929684bb75712e3ff7e3da6a99b4f8d3aa
David van 't Wout <david.vtwout@gmail.com>
2015-10-22 20:24:11 +0200

    started assignment 3.2

--- Homework 3/as_3_1.py
+++ Homework 3/as_3_1.py
@@ -1,6 +1,5 @@
 from scipy.io import loadmat
 from sklearn import tree
-# from sklearn.externals.six import StringIO
 
 # 3.1.1
 filename = 'Data/wine.mat'
@@ -13,14 +12,17 @@ for index in range(len(data['classNames'])):
     classNames.append(data['classNames'][index][0][0])
 for index in range(len(data['attributeNames'][0])):
     attributeNames.append(data['attributeNames'][0][index][0])
+
 # 3.1.2
 clf = tree.DecisionTreeClassifier(criterion="gini", min_samples_split=100)
 clf.fit(attributes, wineClass)
 with open("iris.dot", 'w') as f:
     f = tree.export_graphviz(clf, out_file=f)
+
 # 3.1.3
 wineData = [6.9, 1.09, 0.06, 2.1, 0.0061, 12, 31, 0.99, 3.5, 0.64, 12]
 print("The wine from assignment 3.1.3 is classified as", classNames[clf.predict(wineData)])
+
 # 3.1.4
 good = 0
 bad = 0
--- Homework 3/as_3_2.py
+++ Homework 3/as_3_2.py
@@ -1,1 +1,53 @@
-__author__ = 'dwout'
+from scipy.io import loadmat
+from sklearn import tree
+from sklearn import cross_validation
+import matplotlib.pyplot as plt
+
+filename = 'Data/wine.mat'
+data = loadmat(filename)
+classNames = []
+attributeNames = []
+
+for index in range(len(data['classNames'])):
+    classNames.append(data['classNames'][index][0][0])
+for index in range(len(data['attributeNames'][0])):
+    attributeNames.append(data['attributeNames'][0][index][0])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(data['X'], data['y'])
+
+# 3.2.1
+error = []
+for depth in range(2, 21):
+    clf = tree.DecisionTreeClassifier(criterion="gini", min_samples_split=100, max_depth=depth)
+    clf.fit(X_train, y_train)
+    good = 0
+    bad = 0
+    for i, sample in enumerate(X_test):
+        if clf.predict(sample) == y_test[i]:
+            good += 1
+        else:
+            bad += 1
+    error.append(good/(good+bad)*100)
+
+# plt.scatter(range(2, 21), error)
+# plt.show()
+
+# 3.2.2
+averageError = []
+for depth in range(2, 21):
+    error = []
+    for i in range(100):
+        X_train, X_test, y_train, y_test = cross_validation.train_test_split(data['X'], data['y'])
+        clf = tree.DecisionTreeClassifier(criterion="gini", min_samples_split=100, max_depth=depth)
+        clf.fit(X_train, y_train)
+        good = 0
+        bad = 0
+        for i, sample in enumerate(X_test):
+            if clf.predict(sample) == y_test[i]:
+                good += 1
+            else:
+                bad += 1
+        error.append(good/(good+bad)*100)
+    averageError.append(sum(error)/len(error))
+plt.scatter(range(2, 21), averageError)
+plt.show()
