92fedbc801f9d30f5d289011429d345ca9fab7af
Mattijn <mattijn.k@gmail.com>
2015-10-25 22:19:26 +0100

    tree alspdf

--- Homework 3/as_3_1.py
+++ Homework 3/as_3_1.py
@@ -1,8 +1,7 @@
 from __future__ import print_function
 from scipy.io import loadmat
 from sklearn import tree
-
-
+from PythonScripts import pydot, treeViewer
 # 3.1.1
 filename = 'Data/wine.mat'
 data = loadmat(filename)
@@ -18,11 +17,13 @@ for index in range(len(data['attributeNames'][0])):
 # 3.1.2
 clf = tree.DecisionTreeClassifier(criterion="gini", min_samples_split=100)
 clf.fit(attributes, wineClass)
+treeViewer.view(clf)
 with open("iris.dot", 'w') as f:
-    f = tree.export_graphviz(clf, out_file=f)
+    f = tree.export_graphviz(clf, out_file=f, feature_names=attributeNames)
 
 # 3.1.3
 wineData = [6.9, 1.09, 0.06, 2.1, 0.0061, 12, 31, 0.99, 3.5, 0.64, 12]
+predicted = clf.predict(wineData)
 print("The wine from assignment 3.1.3 is classified as", classNames[clf.predict(wineData)])
 
 # 3.1.4
